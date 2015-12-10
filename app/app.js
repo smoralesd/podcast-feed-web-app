f8726581d9c6d23964cb646370dd1db1d2dc611e
Sebastian Morales <smorales@dena.jp>
2015-12-10 10:00:50 -0300

    Add video list (using mock video list). Add selected video title and description

--- app/app.js
+++ app/app.js
@@ -1,12 +1,36 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+var VideoFeedApp = angular.module('VideoFeedApp', []);
+
+VideoFeedApp.controller('VideoAppCtrl', ['$scope', function($scope) {
+    var videos = [{
+        id: 1,
+        name: 'video 1',
+        description: 'description 1',
+        publishDate: '2015/12/11',
+        img: 'url',
+        active: 'active'
+    }, {
+        id: 2,
+        name: 'video 2',
+        description: 'description 2',
+        publishDate: '2015/12/10',
+        img: 'url'
+    }, {
+        id: 3,
+        name: 'video 3',
+        description: 'description 3',
+        publishDate: '2015/12/09',
+        img: 'url'
+    }];
+
+    $scope.videos = videos;
+
+    $scope.feedUrl = '';
+
+    $scope.selectedVideo = videos[0];
+
 }]);
+
+

ec4360a061988ae4be2a956eb68aa1b6058ab827
Sebastian Morales <smorales@dena.jp>
2015-12-11 21:33:53 -0300

    add audio player and bind enter key to play/pause action

--- app/app.js
+++ app/app.js
@@ -1,53 +1,66 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-var VideoFeedApp = angular.module('VideoFeedApp', []);
+var PodcastFeedApp = angular.module('PodcastFeedApp', []);
 
-VideoFeedApp.filter('trusted', ['$sce', function($sce) {
+PodcastFeedApp.filter('trusted', ['$sce', function($sce) {
     return function(url) {
         return $sce.trustAsResourceUrl(url);
     }
 }])
 
-VideoFeedApp.controller('VideoAppCtrl', ['$scope', 'FeedService', function($scope, FeedService) {
+PodcastFeedApp.controller('PodcastAppCtrl', ['$scope', 'FeedService', function($scope, FeedService) {
 
-    var MAX_VIDEOS = 4;
-    var videos = [];
+    var MAX_PODCASTS = 4;
+    var podcasts = [];
     var selectedIndex = 0;
 
-    FeedService.getVideos().then(function(_videos) {
-        videos = _videos;
-        $scope.videos = videos.slice(0, MAX_VIDEOS);
+    FeedService.getPodcasts().then(function(_podcasts) {
+        podcasts = _podcasts;
+        $scope.podcasts = podcasts.slice(0, MAX_PODCASTS);
 
-        $scope.selectedVideo = videos[selectedIndex];
+        $scope.selectedPodcast = podcasts[selectedIndex];
 
-        updateVideoList(1, 0);
+        updatePodcastsList(1, 0);
     });
 
+    /**
+     * To handle the keyboard events
+     */
     $scope.handleKey = function(event) {
         switch (event.keyCode) {
+            //use k to go down as in vim
             case KEY_VALUES.LOWER_K:
             case KEY_VALUES.UP:
                 handleUp();
                 break;
+            //use j to go up as in vim
             case KEY_VALUES.LOWER_J:
             case KEY_VALUES.DOWN:
                 handleDown();
                 break;
             case KEY_VALUES.ENTER:
-                console.log('handling enter');
+                handleEnter();
                 break;
+        }
+    }
+
+    var handleEnter = function() {
+        var podcast = angular.element(document.querySelector('#podcastPlayer'))[0];
+        if (podcast.paused) {
+            podcast.play();
+        } else {
+            podcast.pause();
         }
     }
 
     var handleDown = function() {
         var previousIndex = selectedIndex++;
 
-        if (selectedIndex >= videos.length) {
-            selectedIndex = videos.length - 1;
+        if (selectedIndex >= podcasts.length) {
+            selectedIndex = podcasts.length - 1;
         }
 
-        updateVideoList(previousIndex, selectedIndex);
+        updatePodcastsList(previousIndex, selectedIndex);
     }
 
     var handleUp = function() {
@@ -57,24 +70,24 @@ VideoFeedApp.controller('VideoAppCtrl', ['$scope', 'FeedService', function($scop
             selectedIndex = 0;
         }
 
-        updateVideoList(previousIndex, selectedIndex);
+        updatePodcastsList(previousIndex, selectedIndex);
     }
 
-    var updateVideoList = function(previous, index) {
-        $scope.selectedVideo.active = null;
-        $scope.selectedVideo = videos[index];
-        $scope.selectedVideo.active = 'active';
+    var updatePodcastsList = function(previous, index) {
+        $scope.selectedPodcast.active = null;
+        $scope.selectedPodcast = podcasts[index];
+        $scope.selectedPodcast.active = 'active';
 
         if (previous != index) {
-            var interval = Math.floor(index / MAX_VIDEOS);
-            var initialPosition = interval * MAX_VIDEOS;
-            var finalPosition = initialPosition + MAX_VIDEOS;
-            $scope.videos = videos.slice(initialPosition, finalPosition);
+            var interval = Math.floor(index / MAX_PODCASTS);
+            var initialPosition = interval * MAX_PODCASTS;
+            var finalPosition = initialPosition + MAX_PODCASTS;
+            $scope.podcasts = podcasts.slice(initialPosition, finalPosition);
         }
     }
 }]);
 
-VideoFeedApp.factory('FeedService', ['$http', '$sce', function($http, $sce) {
+PodcastFeedApp.factory('FeedService', ['$http', '$sce', function($http, $sce) {
     var FeedService = function() {
         this.feeds = {
             'all': 'http://edition.cnn.com/services/podcasting/all/rss.xml',
@@ -90,35 +103,37 @@ VideoFeedApp.factory('FeedService', ['$http', '$sce', function($http, $sce) {
     };
 
     FeedService.prototype._parseFeed = function() {
+        //using google api service to parse the rss feed
         return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(this.getCurrentUrl()));
     };
 
-    FeedService.prototype.getVideos = function() {
+    FeedService.prototype.getPodcasts = function() {
         return this._parseFeed().then(function(results) {
             var entries = results.data.responseData.feed.entries;
             console.log('entries:', entries);
 
-            var videos = entries.map(function(elem) {
-                var newVideo = new VideoModel();
-                newVideo.parse(elem, $sce);
-                return newVideo;
+            var podcasts = entries.map(function(elem) {
+                var newPodcast = new PodcastModel();
+                newPodcast.parse(elem, $sce);
+                return newPodcast;
             });
 
-            return Promise.resolve(videos);
+            return Promise.resolve(podcasts);
         });
     }
 
     return new FeedService();
 }]);
 
-var VideoModel = function() {
+var PodcastModel = function() {
     this.name = 'no name';
     this.description = 'no description';
     this.publishedDate = new Date();
-    this.videoUrl = null;
+    this.url = null;
+    this.active = null;
 };
 
-VideoModel.prototype.parse = function(data, sanitizer) {
+PodcastModel.prototype.parse = function(data, sanitizer) {
     if (data.hasOwnProperty('title')) {
         this.name = data.title;
     }
@@ -128,21 +143,19 @@ VideoModel.prototype.parse = function(data, sanitizer) {
     }
 
     if (data.hasOwnProperty('contentSnippet')) {
-        // this.description = data.contentSnippet;
         this.description = sanitizer.trustAsHtml(data.contentSnippet);
     }
 
     if (data.hasOwnProperty('link')) {
-        this.videoUrl = data.link;
+        this.url = data.link;
     }
 }
 
 var KEY_VALUES = {
-    LEFT: 37,
     UP: 38,
-    RIGHT: 39,
     DOWN: 40,
     LOWER_J: 74,
     LOWER_K: 75,
     ENTER: 13
 };
+
